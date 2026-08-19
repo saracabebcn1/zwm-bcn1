b72193116a6ed3ef7b36d55059c311f54dfa41b8
Marquez Cabello <saracabe@amazon.com>
2026-08-19 14:26:17 +0200

    add Dockerfile

--- app.py
+++ app.py
@@ -291,7 +291,8 @@ with app.app_context():
     init_db()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
 
 @app.context_processor
 def inject_now():
